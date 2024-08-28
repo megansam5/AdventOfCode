873c94ae292873cbf94e5dba9f0d7a5abde9ddfe
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-08-28 13:23:12 +0100

    full first day

--- 2021/day-1/main.py
+++ 2021/day-1/main.py
@@ -1,9 +1,14 @@
+"""Gets the number of increases from a text file."""
+
+
 def read_input(filename: str) -> list[str]:
-    with open(filename, "r") as f:
+    """Reads the file as a list"""
+    with open(filename, "r", encoding="UTF-8") as f:
         return f.readlines()
 
 
 def count_increases(data) -> int:
+    """Counts the number of times the next number increases."""
     count = 0
     for i, depth in enumerate(data):
         if depth > data[i-1] and i != 0:
@@ -11,9 +16,21 @@ def count_increases(data) -> int:
     return count
 
 
+def three_sliding(data) -> list[int]:
+    """Creates a list of 3 sliding window sums."""
+    result = []
+    final_index = len(data) - 3
+    for i, num in enumerate(data):
+        if i <= final_index:
+            total = num + data[i+1] + data[i+2]
+            result.append(total)
+    return result
+
+
 if __name__ == "__main__":
-    data = read_input('data.txt')
+    file_data = read_input('data.txt')
     int_data = []
-    for row in data:
+    for row in file_data:
         int_data.append(int(row))
     print(count_increases(int_data))
+    print(count_increases(three_sliding(int_data)))
