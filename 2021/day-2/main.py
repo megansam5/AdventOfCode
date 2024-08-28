65ad14e127a56b8cd6c5809f1211b3c8be236349
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-08-28 13:51:02 +0100

    day 2 part 2

--- 2021/day-2/main.py
+++ 2021/day-2/main.py
@@ -25,6 +25,27 @@ def calculate_position(data) -> int:
     return depth * horizontal
 
 
+def calculate_position_with_aim(data) -> int:
+    """Calculates the depth and horizontal by using the aim."""
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for row in data:
+        split = row.split()
+        direction = split[0]
+        amount = int(split[1])
+        if direction == 'down':
+            aim += amount
+        elif direction == 'up':
+            aim -= amount
+        else:
+            horizontal += amount
+            depth += (aim * amount)
+
+    return depth * horizontal
+
+
 if __name__ == "__main__":
     instructions = read_input("data.txt")
     print(calculate_position(instructions))
+    print(calculate_position_with_aim(instructions))
