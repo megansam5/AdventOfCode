2b7c3d18c58bd5b3f29e64ce1602ba210455a44c
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-08-30 21:22:39 +0100

    2021 day 3 part 2

--- 2021/day-3/main.py
+++ 2021/day-3/main.py
@@ -9,12 +9,50 @@ def read_input(filename: str) -> list[str]:
 
 def most_common(lst: list):
     """Returns the most common element of a list."""
+    if lst.count('1') == lst.count('0'):
+        return '1'
     return max(set(lst), key=lst.count)
+
+
+def least_common(lst: list):
+    """Returns the least common element of a list."""
+    if lst.count('1') == lst.count('0'):
+        return '0'
+    return min(set(lst), key=lst.count)
 
 
 def get_list_from_index(index: int, lst: list) -> list:
     """Gets the element as an index for a list."""
     return [row[index] for row in lst]
+
+
+def get_list_only_with_values(data: list[str], index: int, most_popular: str) -> list[str]:
+    """Returns the list but only with the value at the index."""
+    return [number for number in data if number[index] == most_popular]
+
+
+def get_oxygen_generator_rating(data: list[str]):
+    """Returns the oxygen generator rating."""
+    binary_length = len(data[0]) - 1
+    result = ""
+    for i in range(binary_length):
+        index_list = get_list_from_index(i, data)
+        most_common_digit = most_common(index_list)
+        data = get_list_only_with_values(data, i, most_common_digit)
+        result += most_common_digit
+    return result
+
+
+def get_co2_scrubber_rating(data: list[str]):
+    """Returns tCO2 scrubber rating."""
+    binary_length = len(data[0]) - 1
+    result = ""
+    for i in range(binary_length):
+        index_list = get_list_from_index(i, data)
+        least_common_digit = least_common(index_list)
+        data = get_list_only_with_values(data, i, least_common_digit)
+        result += least_common_digit
+    return result
 
 
 def get_gamma_ray(data: list[str]):
@@ -44,9 +82,16 @@ def multiply_rates(gamma: str, epsilon: str) -> int:
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def get_life_support_rating(oxygen_generator, co2_scrubber):
+    return int(oxygen_generator, 2) * int(co2_scrubber, 2)
+
+
 if __name__ == "__main__":
     binary = read_input('data.txt')
     gamma_ray = get_gamma_ray(binary)
     epsilon_rate = get_epsilon_rate(gamma_ray)
+    oxygen = get_oxygen_generator_rating(binary)
+    co2 = get_co2_scrubber_rating(binary)
 
     print(multiply_rates(gamma_ray, epsilon_rate))
+    print(get_life_support_rating(oxygen, co2))
